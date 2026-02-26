845f16e63de50f1bbcb95faef3420602fc4f8157
Amine Harch el korane <95189778+Vitalcheffe@users.noreply.github.com>
2026-02-26 10:45:00 +0000

    feat(types): add aerodynamic profiles and speed envelopes

--- aegis/oc_types.py
+++ aegis/oc_types.py
@@ -4,3 +4,10 @@ from dataclasses import dataclass
 
 G_EARTH = 9.80665
 MISSION_ALT_M = 300.0
+
+RHO_300M = 1.1901
+DRONE_S_REF = 0.42
+DRONE_CD0 = 0.024
+DRONE_K = 0.1738
+V_PATROL_MS = 27.5 # 99 km/h (temp)
+V_SPRINT_MS = 88.89 # 320 km/h
