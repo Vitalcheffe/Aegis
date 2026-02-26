d4e49a8675f5945efa9fd95514436bcdaeb902d8
Amine Harch el korane <95189778+Vitalcheffe@users.noreply.github.com>
2026-02-26 11:07:00 +0000

    feat(types): implement Vec3 geometry and TargetClass enums

--- aegis/oc_types.py
+++ aegis/oc_types.py
@@ -11,3 +11,15 @@ DRONE_CD0 = 0.024
 DRONE_K = 0.1738
 V_PATROL_MS = 27.5 # 99 km/h (temp)
 V_SPRINT_MS = 88.89 # 320 km/h
+
+class TargetClass(IntEnum):
+    UNKNOWN = 0
+    PROBABLE = 1
+    CONFIRMED = 2
+    LURE = 3
+
+@dataclass
+class Vec3:
+    x: float; y: float; z: float
+    def dist(self, other: 'Vec3') -> float:
+        return math.sqrt((self.x-other.x)**2 + (self.y-other.y)**2 + (self.z-other.z)**2)
