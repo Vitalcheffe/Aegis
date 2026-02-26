051149819e9639b6ff4fde82356fe5175c457ac1
Amine Harch el korane <95189778+Vitalcheffe@users.noreply.github.com>
2026-02-26 11:56:00 +0000

    feat(fusion): bootstrap FusedTarget data structure

--- aegis/core/fusion.py
+++ aegis/core/fusion.py
@@ -1,46 +1,8 @@
-import time
-class SpectralFusion:
-    def __init__(self):
-        self.CLUSTER_RADIUS = 18.0
-    def is_lure(self, data):
-        return data.ir_signature < 0.5
-# Logic update 1
-# Logic update 2
-# Logic update 3
-# Logic update 4
-# Logic update 5
-# Logic update 6
-# Logic update 7
-# Logic update 8
-# Logic update 9
-# Logic update 10
-# Logic update 11
-# Logic update 12
-# Logic update 13
-# Logic update 14
-# Logic update 15
-# Logic update 16
-# Logic update 17
-# Logic update 18
-# Logic update 19
-# Logic update 20
-# Logic update 21
-# Logic update 22
-# Logic update 23
-# Logic update 24
-# Logic update 25
-# Logic update 26
-# Logic update 27
-# Logic update 28
-# Logic update 29
-# Logic update 30
-# Logic update 31
-# Logic update 32
-# Logic update 33
-# Logic update 34
-# Logic update 35
-# Logic update 36
-# Logic update 37
-# Logic update 38
-# Logic update 39
-# Logic update 40
+from typing import List
+from aegis.oc_types import Vec3, TargetClass
+
+class FusedTarget:
+    def __init__(self, tid: str, pos: Vec3):
+        self.target_id = tid
+        self.pos = pos
+        self.votes = 1
