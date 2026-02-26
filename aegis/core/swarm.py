5dd12f1f79d4dbe0757f33af43db95828b97fbe9
Amine Harch el korane <95189778+Vitalcheffe@users.noreply.github.com>
2026-02-26 16:58:00 +0000

    feat(swarm): define ElasticNet Hooke parameters

--- aegis/core/swarm.py
+++ aegis/core/swarm.py
@@ -1,2 +1,8 @@
+import numpy as np
+from aegis.oc_types import Vec3
+
 class ElasticNet:
-    def __init__(self): self.k = 0.12
+    def __init__(self):
+        self.spacing = 45.0
+        self.k_spring = 0.12
+        self.k_repulse = 500.0
