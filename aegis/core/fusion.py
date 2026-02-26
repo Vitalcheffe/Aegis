85f7e01954f5a7d45118ef113af8829726e15157
Amine Harch el korane <95189778+Vitalcheffe@users.noreply.github.com>
2026-02-26 12:46:00 +0000

    feat(fusion): initialize SpectralFusion state manager

--- aegis/core/fusion.py
+++ aegis/core/fusion.py
@@ -6,3 +6,9 @@ class FusedTarget:
         self.target_id = tid
         self.pos = pos
         self.votes = 1
+
+class SpectralFusion:
+    def __init__(self):
+        self.targets: List[FusedTarget] = []
+        self.next_id = 100
+        self.CLUSTER_RADIUS = 25.0
