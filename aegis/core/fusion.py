0d90570e19ad2cd7d4d49e2c014290a653611a71
Amine Harch el korane <95189778+Vitalcheffe@users.noreply.github.com>
2026-03-05 16:45:00 +0000

    feat(fusion): implement R2 lure rejection based on IR contrast

--- aegis/core/fusion.py
+++ aegis/core/fusion.py
@@ -1,3 +1,6 @@
+import time
 class SpectralFusion:
-    def __init__(self): self.targets = []
-    def add(self, p): self.targets.append(p)
+    def __init__(self):
+        self.CLUSTER_RADIUS = 18.0
+    def is_lure(self, data):
+        return data.ir_signature < 0.5
